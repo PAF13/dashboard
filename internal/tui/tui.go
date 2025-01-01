b6d7d9e0060876861d29bc66b9dbb8baf15dc2f5
Thomas Pfau <pfau.thomas@pro-tags.com>
2025-01-01 02:54:38 +0100

    Auto Update | Version: v0019

---
+++ internal/tui/tui.go
@@ -0,0 +1,44 @@
+package tui
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+const listHeight = 14
+
+var (
+	titleStyle        = lipgloss.NewStyle().MarginLeft(2)
+	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
+	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
+	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
+	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
+	quitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
+)
+
+func TempFunc() {
+
+	items := newItems()
+
+	const defaultWidth = 20
+
+	l := list.New(*items, itemDelegate{}, 100, 21)
+	l.Title = ""
+	l.SetShowStatusBar(false)
+	l.SetFilteringEnabled(false)
+	l.Styles.Title = titleStyle
+	l.Styles.PaginationStyle = paginationStyle
+	l.Styles.HelpStyle = helpStyle
+
+	m := model{list: l}
+
+	if _, err := tea.NewProgram(m, tea.WithAltScreen()).Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
+
+}
--- main.go
+++ main.go
@@ -1,63 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
-	"time"
 
 	"github.com/PAF13/dashboard/internal/httpserver"
-	"github.com/charmbracelet/bubbles/list"
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
-)
-
-const listHeight = 14
-
-var (
-	titleStyle        = lipgloss.NewStyle().MarginLeft(2)
-	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
-	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
-	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
-	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
-	quitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
+	"github.com/PAF13/dashboard/internal/tui"
 )
 
 func main() {
-	//neondb.GetTrans()
-	// Start logging server
 	log.Println("starting")
 	logBuf := &httpserver.LogBuffer{}
 	log.SetOutput(logBuf) // Redirect log output to the buffer.
 	go httpserver.Server(logBuf)
 
-	log.Println("moving on")
-	time.Sleep(1000 * time.Second)
-
-	if false {
-		tempFunc()
+	if true {
+		tui.TempFunc()
 	}
-}
-
-func tempFunc() {
-
-	items := newItems()
-
-	const defaultWidth = 20
-
-	l := list.New(*items, itemDelegate{}, 100, 21)
-	l.Title = ""
-	l.SetShowStatusBar(false)
-	l.SetFilteringEnabled(false)
-	l.Styles.Title = titleStyle
-	l.Styles.PaginationStyle = paginationStyle
-	l.Styles.HelpStyle = helpStyle
-
-	m := model{list: l}
-
-	if _, err := tea.NewProgram(m, tea.WithAltScreen()).Run(); err != nil {
-		fmt.Println("Error running program:", err)
-		os.Exit(1)
-	}
-
 }
